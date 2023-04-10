21cee7639507ec2db924797368b03274bd88000b
Joe W <whobbes22@gmail.com>
2023-04-10 16:01:21 -0700

    add randomize deck and build deck prototypes

--- src/memory.js
+++ src/memory.js
@@ -4,8 +4,17 @@ function Deck(cardPairs){
   this.deckArray = []; 
 }
 
+let deck = new Deck(5); // global variable to be deleted?
+
 Deck.prototype.buildDeck = function(){
-  for(let i =1; i<=this.cardPairs; i= i+.5){
-    this.deckArray.push(Math.floor(i));
+  let arr = [];
+  const len = this.cardpairs; 
+  for(let i =1; i<=len+.5; i= i+.5){
+    arr.push(Math.floor(i));
   }
+  this.deckArray = arr;
+};
+
+Deck.prototype.randomizeDeck() = function(){
+  console.log(this.deckArray);
 }
